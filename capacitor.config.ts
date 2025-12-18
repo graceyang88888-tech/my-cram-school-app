da993adb305e7b8e39188c5f584713345c8905f0
Grace Yang <graceyang88888@gmail.com>
2025-12-18 10:50:01 +0800

    Add Apple touch icon configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.graceyang.app',
+  appName: 'my-cram-school-app',
+  webDir: 'public'
+};
+
+export default config;
